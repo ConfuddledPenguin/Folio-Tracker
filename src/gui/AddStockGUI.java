7f2ad13e40bced20677b72d86918c23d4b043dda
RyanGibson20 <RyanGibson20@users.noreply.github.com>
2014-12-13 14:24:48 +0000

    Added Javadoc

--- src/gui/AddStockGUI.java
+++ src/gui/AddStockGUI.java
@@ -22,25 +22,29 @@ public class AddStockGUI {
 	private JFrame frame;
 	private JPanel mainPanel;
 	private JTextField nameField;
-	
+
 	private ActionListener al;
-	
+
 	private Portfolio portfolio;
 
 	public AddStockGUI(Portfolio portfolio) {
-		
+
 		this.portfolio = portfolio;
-		
+
 		makeFrame();
 	}
 
+	/**
+	 * Creates the initial frame before adding a button to the bottom and the
+	 * stock panel to the center
+	 */
 	private void makeFrame() {
 		frame = new JFrame("Add new Stock");
 		frame.setSize(400, 175);
 		frame.setResizable(false);
 		// allow the frame to be in front of the home GUI
 		frame.setAlwaysOnTop(true);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		// centre the GUI according to the screen size
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
@@ -50,30 +54,41 @@ public class AddStockGUI {
 		mainPanel.setPreferredSize(new Dimension(400, 175));
 		createStockPanel();
 		JButton add = new JButton("Add Stock");
-		add.addActionListener( new AddStockGUIListener(this, portfolio) );
+		add.addActionListener(new AddStockGUIListener(this, portfolio));
 		mainPanel.add(add, BorderLayout.SOUTH);
 		frame.add(mainPanel);
 		frame.revalidate();
 
 	}
-	
-	private void createStockPanel(){
-		JPanel stockPanel = new JPanel(new GridLayout(1,1));
+
+	/**
+	 * Build the stock Panel consisting of a label and text field
+	 */
+	private void createStockPanel() {
+		JPanel stockPanel = new JPanel(new GridLayout(1, 1));
 		stockPanel.setBorder(new EmptyBorder(50, 80, 50, 80));
-		JLabel label =  new JLabel("Name Of stock:");
+		JLabel label = new JLabel("Name Of stock:");
 		JTextField textField = new JTextField(20);
 		stockPanel.add(label);
 		stockPanel.add(textField);
 		mainPanel.add(stockPanel, BorderLayout.CENTER);
-		
+
 	}
-	
-	public String getTicker(){
+    
+	/**
+	 * Simple getter method to return the ticker name
+	 * 
+	 * @return - ticker name
+	 */
+	public String getTicker() {
 		return nameField.getText();
 	}
-	
-	public void close(){
+    
+	/**
+	 * Method to close the JFrame
+	 */
+	public void close() {
 		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 	}
-	
+
 }
