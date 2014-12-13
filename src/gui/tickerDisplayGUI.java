cfed05e9b66ffaa9d9602764ba64fc83d845baac
RyanGibson20 <RyanGibson20@users.noreply.github.com>
2014-12-13 16:08:58 +0000

    changed the layout
    Now uses the grid layout

--- src/gui/tickerDisplayGUI.java
+++ src/gui/tickerDisplayGUI.java
@@ -3,46 +3,36 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class tickerDisplayGUI {
+public class newTickerDisplayGUI {
+
 	private JFrame frame;
 	private JPanel mainPanel;
-	private JPanel tickerPanel;
-	private JPanel valuePanel;
-	private JPanel gainPanel;
-	private JMenuBar menus;
-	private JMenu edit;
-	private JMenuItem addShares;
-	private JMenuItem sellShares;
-	private JLabel tickerSymbol;
-	private JLabel stockName;
-	private JLabel currentValue;
-	private JLabel dailyChange;
-	private JLabel totalSpend;
-	private JLabel totalGain;
-	
 
 	public static void main(String[] args) {
 
-		new tickerDisplayGUI();
+		new newTickerDisplayGUI();
 	}
 
-	public tickerDisplayGUI() {
+	public newTickerDisplayGUI() {
 		makeFrame();
 	}
-	
-	private void makeFrame(){
+
+	/**
+	 * creates the frame which displays the ticker information as well as
+	 * buttons that allow the user to sell and buy shares for that ticker
+	 */
+	private void makeFrame() {
 		// create a new frame
-		frame = new JFrame("Editing Portfolio 'tickerName' for 'portfolioName'");
+		frame = new JFrame("Editing Portfolio 'tickerName' for 'portfolioName");
 		frame.setSize(500, 300);
 		// frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,65 +42,49 @@ public class tickerDisplayGUI {
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(d.width / 2 - frame.getWidth() / 2, d.height / 2
 				- frame.getHeight() / 2);
-		createMenu(frame);
 		mainPanel = new JPanel(new BorderLayout());
 		mainPanel.setPreferredSize(new Dimension(500, 300));
 		createTickerPanel();
-		createValuePanel();
-		createGainPanel();
+		createButtonPanel();
 		frame.add(mainPanel);
 		frame.revalidate();
 		frame.repaint();
 	}
-	
-	private void createMenu(JFrame frame){
-			menus = new JMenuBar();
-			edit = new JMenu("Edit");
-			addShares = new JMenuItem("Add Shares");
-			sellShares = new JMenuItem("Sell Shares");
-			menus.add(edit);
-			edit.add(addShares);
-			edit.add(sellShares);
-			frame.setJMenuBar(menus);
-		}
-		
-	
-	private void createTickerPanel(){
-		tickerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		Dimension d = new Dimension(500, 100);
-		tickerPanel.setPreferredSize(d);
-		tickerPanel.setBorder(new EmptyBorder(20, 0, 0, 0));
-		tickerSymbol = new JLabel("Ticker Symbol: +Ticker Symbol");
-		tickerSymbol.setBorder(new EmptyBorder(0, 0, 0, 30));
-		stockName = new JLabel("Stock Name: +StockName");
-		tickerPanel.add(tickerSymbol);
-		tickerPanel.add(stockName);
-		mainPanel.add(tickerPanel, BorderLayout.NORTH);
+
+	/**
+	 * creates the panel which displays all the information for that ticker.
+	 * This panel is then centered in the main frame.
+	 */
+	private void createTickerPanel() {
+		JPanel tickerPanel = new JPanel(new GridLayout(6, 0, 10, 0));
+		tickerPanel.setBorder(new EmptyBorder(10, 180, 0, 0));
+		JLabel tickerSymbolLabel = new JLabel("Ticker Symbol: +tickerSymbol");
+		JLabel stockNameLabel = new JLabel("Stock Name: +stockName");
+		JLabel currentValueLabel = new JLabel("Current Value: +currentValue");
+		JLabel dailyChangeLabel = new JLabel("Daily Change: +dailyChange");
+		JLabel totalGainLabel = new JLabel("Total Gain: +totalGain");
+		JLabel totalSpend = new JLabel("Total Spend: + totalSpend");
+		tickerPanel.add(tickerSymbolLabel);
+		tickerPanel.add(stockNameLabel);
+		tickerPanel.add(currentValueLabel);
+		tickerPanel.add(dailyChangeLabel);
+		tickerPanel.add(totalGainLabel);
+		tickerPanel.add(totalSpend);
+		mainPanel.add(tickerPanel, BorderLayout.CENTER);
 	}
-	
-	private void createValuePanel(){
-		valuePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		Dimension d = new Dimension(500, 100);
-		valuePanel.setPreferredSize(d);
-		currentValue = new JLabel("Current Value: +Current Value");
-		currentValue.setBorder(new EmptyBorder(0, 0, 0, 30));
-		dailyChange = new JLabel("Daily Change: +Daily Change");
-		valuePanel.add(currentValue);
-		valuePanel.add(dailyChange);
-		mainPanel.add(valuePanel, BorderLayout.CENTER);
+
+	/**
+	 * Creates the panel which consists of the add and sell shares buttons. This
+	 * panel is set to the south in the main frame
+	 */
+	private void createButtonPanel() {
+		JPanel buttonPanel = new JPanel(new FlowLayout());
+		buttonPanel.setBorder(new EmptyBorder(10, 0, 20, 0));
+		JButton buyShares = new JButton("Buy Shares");
+		JButton sellShares = new JButton("Sell Shares");
+		buttonPanel.add(buyShares);
+		buttonPanel.add(sellShares);
+		mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 	}
-	
-	
-	private void createGainPanel(){
-		gainPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		Dimension d = new Dimension(500, 100);
-		valuePanel.setPreferredSize(d);
-		gainPanel.setBorder(new EmptyBorder(0, 0, 30, 0));
-		totalGain = new JLabel("Total Gain: +totalGain");
-		totalGain.setBorder(new EmptyBorder(0, 0, 0, 30));
-		totalSpend = new JLabel("Total Spend: +totalSpend");
-		gainPanel.add(totalGain);
-		gainPanel.add(totalSpend);
-		mainPanel.add(gainPanel, BorderLayout.SOUTH);
-	}
+
 }
