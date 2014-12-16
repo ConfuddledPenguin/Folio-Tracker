ab179b2acb29fd9c16ea1604928fd8cb98293365
Thomas Maxwell <hello@tom-maxwell.com>
2014-12-16 18:59:41 +0000

    add the files tom!!!!

---
+++ src/tracker/CloseFolioListener.java
@@ -0,0 +1,56 @@
+package tracker;
+
+import gui.ConfirmationInterface;
+import gui.HomeGUIInterface;
+import gui.confirmationGUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+
+import model.*;
+
+public class CloseFolioListener implements ActionListener{
+
+	private HomeGUIInterface ui;
+	private Tracker tracker;
+	ConfirmationInterface confirmui = null;
+	
+	public CloseFolioListener(HomeGUIInterface ui, Tracker tracker) {
+		this.ui = ui;
+		this.tracker = tracker;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String source = "";
+		Portfolio portfolio = ui.getCurrentPortfolio();
+		
+		if(portfolio == null){
+			return;
+		}
+		
+		if(e.getSource() instanceof JMenuItem){
+			source = ((JMenuItem) e.getSource()).getText();
+		}else if(e.getSource() instanceof JButton){
+			source = ((JButton) e.getSource()).getText();
+		}
+		
+		if( source.equals("Close Folio")){
+			if(portfolio.isSaved()){
+				tracker.deletePortfolio(portfolio);
+			}else{
+				confirmui = new confirmationGUI("Warning: Portfolio " + portfolio.getName() + "is not saved. Are you sure you wish to close it?", this);
+			}
+		}else if(source.equals("Yes")){
+			System.out.println("close");
+			tracker.getPortfolios();
+			tracker.deletePortfolio(portfolio);
+			confirmui.close();
+		}
+	}
+
+}
---
+++ src/tracker/ErrorGUIListener.java
@@ -0,0 +1,24 @@
+package tracker;
+
+import gui.ErrorInterface;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ErrorGUIListener implements ActionListener{
+
+	private ErrorInterface ui;
+	
+	public ErrorGUIListener(ErrorInterface ui) {
+		this.ui = ui;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ui.close();
+		
+	}
+	
+	
+
+}
